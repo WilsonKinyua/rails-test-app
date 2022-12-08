a63035f8f75294fda4c63e125d5a21502c4ca7b0
Wilson Kinyua <44901716+WilsonKinyua@users.noreply.github.com>
2022-12-08 12:56:57 +0300

    Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  
+  config.hosts << "rails-production-de67.up.railway.app"
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
