442c2836e12ab2cf1ccd41c23dff054fb428f989
Wilson Kinyua <44901716+WilsonKinyua@users.noreply.github.com>
2022-12-08 12:49:26 +0300

    config url

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module RubyRailsPostgres
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.hosts << "rails-production-de67.up.railway.app"
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
